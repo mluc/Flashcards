8cb24e90c635f1bb1efbde8bddf79d07c5b7a48f
My Luc <lucphumy@gmail.com>
2018-02-23 19:51:35 -0600

    clean up

--- mobile-flashcards/utils/_calendar.js
+++ mobile-flashcards/utils/_calendar.js
@@ -1,5 +1,5 @@
-import { AsyncStorage } from 'react-native'
-import { getMetricMetaInfo, timeToString } from './helpers'
+import {AsyncStorage} from "react-native";
+import {timeToString} from "./helpers";
 
 export const CALENDAR_STORAGE_KEY = 'Project:desks'
 
@@ -8,7 +8,7 @@ function getRandomNumber (max) {
 }
 
 function setDummyData () {
-    const { run, bike, swim, sleep, eat } = getMetricMetaInfo()
+
 
     let dummyData = {}
     const timestamp = Date.now()
