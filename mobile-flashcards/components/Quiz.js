ae7f935b65134a5125063756467a266cb41da8a3
My Luc <lucphumy@gmail.com>
2018-02-25 11:55:11 -0600

    move buttons out to a file

--- mobile-flashcards/components/Quiz.js
+++ mobile-flashcards/components/Quiz.js
@@ -4,77 +4,35 @@ import {clearLocalNotification, setLocalNotification, timeToString} from "../uti
 import {connect} from "react-redux";
 import {gray, green, lightBlue, red, white} from "../utils/colors";
 import {NavigationActions} from "react-navigation";
+import {
+    AnswerBtn,
+    QuestionBtn,
+    TakeQuizAgainBtn,
+    BackToDeskBtn,
+    CorrectBtn,
+    IncorrectBtn,
+    NextQuestionBtn
+} from './QuizButtons'
 
-function AnswerBtn({onPress}) {
-    return (
-        <TouchableOpacity
-            style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.AndroidSubmitBtn}
-            onPress={onPress}>
-            <Text style={styles.submitBtnText}>Show Answer</Text>
-        </TouchableOpacity>
-    )
-}
-function QuestionBtn({onPress}) {
-    return (
-        <TouchableOpacity
-            style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.AndroidSubmitBtn}
-            onPress={onPress}>
-            <Text style={styles.submitBtnText}>Show Question</Text>
-        </TouchableOpacity>
-    )
-}
-function TakeQuizAgainBtn({onPress}) {
-    return (
-        <TouchableOpacity
-            style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.AndroidSubmitBtn}
-            onPress={onPress}>
-            <Text style={styles.submitBtnText}>Take Quiz Again</Text>
-        </TouchableOpacity>
-    )
-}
-function BackToDeskBtn({onPress}) {
-    return (
-        <TouchableOpacity
-            style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.AndroidSubmitBtn}
-            onPress={onPress}>
-            <Text style={styles.submitBtnText}>Back To Desk</Text>
-        </TouchableOpacity>
-    )
-}
-
-function CorrectBtn({onPress}) {
-    return (
-        <TouchableOpacity
-            style={[Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.AndroidSubmitBtn, styles.greenBackground]}
-            onPress={onPress}>
-            <Text style={styles.submitBtnText}>Correct</Text>
-        </TouchableOpacity>
-    )
-}
-
-function IncorrectBtn({onPress}) {
-    return (
-        <TouchableOpacity
-            style={[Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.AndroidSubmitBtn, styles.redBackground]}
-            onPress={onPress}>
-            <Text style={styles.submitBtnText}>Incorrect</Text>
-        </TouchableOpacity>
-    )
-}
 class Quiz extends Component {
     static navigationOptions = () => {
-
         return {
             title: 'Quiz'
         }
     }
+
+    componentDidMount() {
+        this.prepareQuestion(Object.keys(this.props.navigation.state.params.cards).length > 0 ? 1 : 0)
+    }
+
     state = {
         questionNum: -1,
         correctCount: 0,
         showQuestion: true,
         isDone: false,
         isCorrect: false,
-        cardQuestion: null
+        cardQuestion: null,
+        cardAnswer : null
     }
     prepareQuestion = (num) => {
         const {cards} = this.props.navigation.state.params
@@ -89,11 +47,6 @@ class Quiz extends Component {
         }))
 
     }
-
-    componentDidMount() {
-        this.prepareQuestion(Object.keys(this.props.navigation.state.params.cards).length > 0 ? 1 : 0)
-    }
-
     answerClick = () => {
         this.setState(() => ({showQuestion: false}))
     }
@@ -108,41 +61,35 @@ class Quiz extends Component {
         }))
         this.prepareQuestion(Object.keys(this.props.navigation.state.params.cards).length > 0 ? 1 : 0)
     }
-
-    checkAnswer = (isDone) => {
-        const {questionNum, isCorrect, correctCount} = this.state
-
-        Object.keys(this.props.navigation.state.params.cards).map((key, index) => {
-            if (index === questionNum - 1) {
-                if (isCorrect) {
-                    this.setState(() => ({correctCount: correctCount + 1}))
-                }
-            }
-        })
+    checkAnswer = (questionNum, cardsArrayLength) => () =>{
+        const {isCorrect, correctCount} = this.state
+        if (isCorrect) {
+            this.setState(() => ({correctCount: correctCount + 1}))
+        }
+        const isDone = questionNum === cardsArrayLength
         if (isDone) {
             this.setState(() => ({isDone: true}))
             clearLocalNotification().then(setLocalNotification)
         }
-
+        this.prepareQuestion(isDone ? 0 : questionNum + 1);
     }
+
     correctBtnClick = () => {
         this.setState(() => ({isCorrect: true}))
     }
-
     incorrectBtnClick = () => {
         this.setState(() => ({isCorrect: false}))
     }
 
     render() {
-
         const {cards} = this.props.navigation.state.params
         const {questionNum, showQuestion, isDone, correctCount, cardQuestion, cardAnswer} = this.state
-        const cardsArray = Object.keys(cards)
+        const cardIdsArray = Object.keys(cards)
         if (isDone) {
             return (
                 <View style={styles.center}>
                     <Text style={{color: lightBlue, fontSize: 25}}>
-                        {correctCount} / {cardsArray.length} correct!
+                        {correctCount} / {cardIdsArray.length} correct!
                     </Text>
                     <View style={{justifyContent: 'center'}}>
                         <TakeQuizAgainBtn onPress={this.takeQuizAgainClick}/>
@@ -157,7 +104,7 @@ class Quiz extends Component {
         return (
             <View style={styles.container}>
                 <Text style={{color: lightBlue, fontSize: 25}}>
-                    {questionNum} / {cardsArray.length}
+                    {questionNum} / {cardIdsArray.length}
                 </Text>
 
                 {showQuestion &&
@@ -198,18 +145,8 @@ class Quiz extends Component {
                         <IncorrectBtn onPress={this.incorrectBtnClick}/>
                     </View>
                     <View style={{justifyContent: 'center'}}>
-                        <TouchableOpacity
-                            style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.AndroidSubmitBtn}
-                            onPress={() => {
-                                const doneQuiz = questionNum === cardsArray.length;
-                                this.prepareQuestion(doneQuiz ? 0 : questionNum + 1);
-                                this.checkAnswer(doneQuiz)
-                            }}>
-                            <Text
-                                style={styles.submitBtnText}>
-                                {questionNum === cardsArray.length ? 'Done' : 'Next Question'}
-                            </Text>
-                        </TouchableOpacity>
+                        <NextQuestionBtn isDone={questionNum===cardIdsArray.length}
+                                         onPress={this.checkAnswer(questionNum, cardIdsArray.length)}/>
                     </View>
                 </View>
 
@@ -226,46 +163,15 @@ const styles = StyleSheet.create({
         justifyContent: 'space-around',
 
     },
-
     row: {
         flexDirection: 'row',
         alignItems: 'center',
         backgroundColor: 'transparent',
     },
-    iosSubmitBtn: {
-        backgroundColor: lightBlue,
-        padding: 10,
-        borderRadius: 7,
-        height: 45,
-        marginLeft: 40,
-        marginRight: 40,
-    },
-    AndroidSubmitBtn: {
-        backgroundColor: lightBlue,
-        padding: 10,
-        paddingLeft: 30,
-        paddingRight: 30,
-        height: 45,
-        borderRadius: 2,
-        alignSelf: 'flex-end',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    redBackground: {
-        backgroundColor: red,
-    },
-    greenBackground: {
-        backgroundColor: green,
-    },
     submitBtnText: {
         color: white,
         fontSize: 22,
         textAlign: 'center',
-    },
-
-    nextQuestionBtn: {
-        textAlign: 'center',
-        color: lightBlue,
     },
     center: {
         flex: 1,
---
+++ mobile-flashcards/components/QuizButtons.js
@@ -0,0 +1,111 @@
+import React, {Component} from "react";
+import {Platform, StyleSheet, Text, TouchableOpacity} from "react-native";
+import {green, lightBlue, red, white} from "../utils/colors";
+
+export function AnswerBtn({onPress}) {
+    return (
+        <TouchableOpacity
+            style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.AndroidSubmitBtn}
+            onPress={onPress}>
+            <Text style={styles.submitBtnText}>Show Answer</Text>
+        </TouchableOpacity>
+    )
+}
+export function QuestionBtn({onPress}) {
+    return (
+        <TouchableOpacity
+            style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.AndroidSubmitBtn}
+            onPress={onPress}>
+            <Text style={styles.submitBtnText}>Show Question</Text>
+        </TouchableOpacity>
+    )
+}
+export function TakeQuizAgainBtn({onPress}) {
+    return (
+        <TouchableOpacity
+            style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.AndroidSubmitBtn}
+            onPress={onPress}>
+            <Text style={styles.submitBtnText}>Take Quiz Again</Text>
+        </TouchableOpacity>
+    )
+}
+export function BackToDeskBtn({onPress}) {
+    return (
+        <TouchableOpacity
+            style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.AndroidSubmitBtn}
+            onPress={onPress}>
+            <Text style={styles.submitBtnText}>Back To Desk</Text>
+        </TouchableOpacity>
+    )
+}
+
+export function CorrectBtn({onPress}) {
+    return (
+        <TouchableOpacity
+            style={[Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.AndroidSubmitBtn, styles.greenBackground]}
+            onPress={onPress}>
+            <Text style={styles.submitBtnText}>Correct</Text>
+        </TouchableOpacity>
+    )
+}
+
+export function IncorrectBtn({onPress}) {
+    return (
+
+        <TouchableOpacity
+            style={[Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.AndroidSubmitBtn, styles.redBackground]}
+            onPress={onPress}>
+            <Text style={styles.submitBtnText}>Incorrect</Text>
+        </TouchableOpacity>
+
+    )
+}
+
+export function NextQuestionBtn({onPress, isDone}) {
+
+    return (
+
+        <TouchableOpacity
+            style={[Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.AndroidSubmitBtn]}
+            onPress={onPress}>
+            <Text style={styles.submitBtnText}>{isDone ? 'Done' : 'Next Question'}</Text>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    iosSubmitBtn: {
+        backgroundColor: lightBlue,
+        padding: 10,
+        borderRadius: 7,
+        height: 45,
+        marginLeft: 40,
+        marginRight: 40,
+    },
+    AndroidSubmitBtn: {
+        backgroundColor: lightBlue,
+        padding: 10,
+        paddingLeft: 30,
+        paddingRight: 30,
+        height: 45,
+        borderRadius: 2,
+        alignSelf: 'flex-end',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    redBackground: {
+        backgroundColor: red,
+    },
+    greenBackground: {
+        backgroundColor: green,
+    },
+    submitBtnText: {
+        color: white,
+        fontSize: 22,
+        textAlign: 'center',
+    },
+    nextQuestionBtn: {
+        textAlign: 'center',
+        color: lightBlue,
+    },
+})
